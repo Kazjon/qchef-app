731483cda1b5968b0068188d4dff11b9d4ee5756
Vivienne Jiang <vivienne@theprojectfactory.com>
2020-05-22 10:35:29 +1000

    refactor header & footer

--- src/app/views/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,9 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardPage implements OnInit {
+  imgSrcRoot = '../../../../../assets/images/'
+  @Input() recipe = this.imgSrcRoot + 'icon-recipes.svg';
+  @Input() recipeInactive = this.imgSrcRoot + 'icon-recipes-inactive.svg';
+  @Input() shoppingList = this.imgSrcRoot + 'icon-shopping-list.svg';
+  @Input() shoppingListInactive = this.imgSrcRoot + 'icon-shopping-list-inactive.svg';
+  @Input() reviews = this.imgSrcRoot + 'icon-review.svg';
+  @Input() reviewsInactive = this.imgSrcRoot + 'icon-review-inactive.svg';
 
-  constructor() { }
+  @Input() isRecipesActive: boolean = false;
+  @Input() isShoppingListActive: boolean = false;
+  @Input() isReviewsActive: boolean = false;
+
+  constructor() { 
+    this.isRecipesActive = true;
+  }
 
   ngOnInit() {}
 
+  changeTabs(tabName: string) {
+    
+    switch(tabName) {
+      case 'recipes': 
+          this.isRecipesActive = true;
+          this.isShoppingListActive = false;
+          this.isReviewsActive = false;
+          break;
+      case 'shoppinglist': 
+          this.isRecipesActive = false;
+          this.isShoppingListActive = true;
+          this.isReviewsActive = false;
+          break;
+      case 'reviews': 
+          this.isRecipesActive = false;
+          this.isShoppingListActive = false;
+          this.isReviewsActive = true;
+          break;
+    }
+  }
 }
