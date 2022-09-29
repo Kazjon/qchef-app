a7c256bbfa0a6e8fb8ad6792ae2ab21b19c89628
Joel Dawson <joel@theprojectfactory.com>
2022-09-29 17:46:24 +1000

    Various tickets completed

--- src/app/views/login/login.page.ts
+++ src/app/views/login/login.page.ts
@@ -1,197 +1,196 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
-import { FirebaseService } from '../../services/firebase/firebase.service';
-import { Router } from '@angular/router';
-import { DataService } from 'src/app/services/data/data.service';
-import { DataHandlingService } from 'src/app/services/datahandling/datahandling.service';
-import { MealSlot } from 'src/app/core/objects/MealSlot';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, ValidationErrors, AbstractControl } from "@angular/forms";
+import { FirebaseService } from "../../services/firebase/firebase.service";
+import { Router } from "@angular/router";
+import { DataService } from "src/app/services/data/data.service";
+import { DataHandlingService } from "src/app/services/datahandling/datahandling.service";
+import { MealSlot } from "src/app/core/objects/MealSlot";
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.page.html',
-    styleUrls: ['./login.page.scss'],
+   selector: "app-login",
+   templateUrl: "./login.page.html",
+   styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
-    loginForm: FormGroup;
-    registerForm: FormGroup;
-    formSubmitted: boolean = false;
-    formDisabled: boolean = false;
-    errorMessage: string = undefined;
-    loginStates = {
-        loading: "loading",
-        ready: "ready",
-        error: "error"
-    }
-    state: string = undefined;
-    switchToRegisterForm: boolean = false;
+   loginForm: FormGroup;
+   registerForm: FormGroup;
+   formSubmitted: boolean = false;
+   formDisabled: boolean = false;
+   errorMessage: string = undefined;
+   loginStates = {
+      loading: "loading",
+      ready: "ready",
+      error: "error",
+   };
+   state: string = undefined;
+   switchToRegisterForm: boolean = false;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private firebaseService: FirebaseService,
-        private router: Router,
-        private dataService: DataService,
-        private dataHandlingService: DataHandlingService) { }
+   constructor(
+      private formBuilder: FormBuilder,
+      private firebaseService: FirebaseService,
+      private router: Router,
+      private dataService: DataService,
+      private dataHandlingService: DataHandlingService
+   ) {}
 
-    ngOnInit() {
+   ngOnInit() {
+      this.createLoginForm();
 
-        this.createLoginForm();
-        
-        this.state = this.loginStates.ready;
+      this.state = this.loginStates.ready;
 
-        let idToken = localStorage.getItem('idToken');
-        if (idToken != undefined) {
-            this.router.navigateByUrl('dashboard/recipes', { replaceUrl: true });
-        }
-    }
+      let idToken = localStorage.getItem("idToken");
+      if (idToken != undefined) {
+         this.router.navigateByUrl("dashboard/recipes", { replaceUrl: true });
+      }
+   }
 
-    segmentChanged(ev: any) {
-        this.state = this.loginStates.ready;
-        this.enableForm();
-        this.errorMessage = "";
-        console.log('Segment changed', ev);
-        this.switchToRegisterForm = !this.switchToRegisterForm;
-        if (this.switchToRegisterForm) {
-            this.createRegisterForm()
-        }
-    }
+   segmentChanged(ev: any) {
+      this.state = this.loginStates.ready;
+      this.enableForm();
+      this.errorMessage = "";
+      console.log("Segment changed", ev);
+      this.switchToRegisterForm = !this.switchToRegisterForm;
+      if (this.switchToRegisterForm) {
+         this.createRegisterForm();
+      }
+   }
 
-    private createLoginForm() {
-        this.loginForm = this.formBuilder.group({
-            email: ['', [
-                Validators.required,
-                Validators.pattern("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"),
-            ]],
-            password: ['', [
-                Validators.required,
-                Validators.minLength(9),
-            ]]
-        });
-    }
+   private createLoginForm() {
+      this.loginForm = this.formBuilder.group({
+         email: [
+            "",
+            [Validators.required, Validators.pattern("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")],
+         ],
+         password: ["", [Validators.required, Validators.minLength(9)]],
+      });
+   }
 
-    private createRegisterForm() {
-        this.registerForm = this.formBuilder.group({
-            email: ['', [
-                Validators.required,
-                Validators.pattern("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
-            ]],
-            password: ['', [
-                Validators.required,
-                Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\/\-=+_<>.,\\\]\[():;"~|{}])[A-Za-z\d@$!%*?&\/\-=+_<>.,\\\]\[():;"~|{}]{8,}$/)
-            ]],
-            confirmPassword: [''],
-        }, {validator: [this.checkPasswords]});
-    }
+   private createRegisterForm() {
+      this.registerForm = this.formBuilder.group(
+         {
+            email: [
+               "",
+               [Validators.required, Validators.pattern("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")],
+            ],
+            password: [
+               "",
+               [
+                  Validators.required,
+                  //Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\/\-=+_<>.,\\\]\[():;"~|{}])[A-Za-z\d@$!%*?&\/\-=+_<>.,\\\]\[():;"~|{}]{8,}$/)
+                  Validators.pattern("^(?=.*?[a-z])(?=.*?[0-9]).{9,}$"),
+               ],
+            ],
+            confirmPassword: [""],
+         },
+         { validator: [this.checkPasswords] }
+      );
+   }
 
-    login() {
+   login() {
+      if (this.loginForm.valid) {
+         this.state = this.loginStates.loading;
+         this.disableForm();
 
-        if (this.loginForm.valid) {
-
-            this.state = this.loginStates.loading;
-            this.disableForm();
-
-            this.firebaseService.loginWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password)
-                .then((res) => {
-                    const firebaseResp = <any>res;
-                    if(firebaseResp && firebaseResp.user && firebaseResp.user.uid){
-                        localStorage.setItem('userID', firebaseResp.user.uid)
-                    }
-                    this.firebaseService.getUserIDToken()
-                        .then((res) => {
-                            this.dataService.getCustomTokenFromServer(res).subscribe((customToken) => {
-                                this.dataService.initAuthToken(customToken['token']);
-                                localStorage.setItem('idToken', customToken['token']);
-                                localStorage.setItem('loginDate',new Date().toUTCString());
-                                this.state = this.loginStates.ready;
-                                this.getSelectedMealPlan();
-                            });
-                        })
-                })
-                .catch((error) => {
-                    this.state = this.loginStates.error;
-                    this.enableForm();
-                    this.errorMessage = error.message;
-                });
-        }
-
-    }
-
-    private getSelectedMealPlan() {
-        this.dataService.getSelectedMealPlanFromServer().subscribe((res) => {
-
-            this.dataHandlingService.handleMealSlotData(res)
-                .then((res: MealSlot[]) => {
-                    if (res) {
-                        this.dataService.setMealSlots(res);
-                        this.router.navigateByUrl('splash');
-                    } else {
-                        this.router.navigateByUrl('onboarding', { replaceUrl: true });
-                    }
-                    this.enableForm();
-                });
-        },
-            (exception) => {
-                // need to check if server return 'Unable to authenticate'
-                if (exception && exception.error && typeof (exception.error) == "string") {
-                    const strRes = <string>exception.error;
-                    if (strRes.includes('Unable to authenticate')) {
-                        this.state = this.loginStates.error;
-                        this.enableForm();
-                        this.errorMessage = "Internal server error";
-                        this.firebaseService.logoutUserFromApp().then(() => {
-                            //this.router.navigateByUrl('login');
-                            
-                        });
-                    }
-                }
-
-                this.router.navigateByUrl('onboarding', { replaceUrl: true });
+         this.firebaseService
+            .loginWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password)
+            .then((res) => {
+               const firebaseResp = <any>res;
+               if (firebaseResp && firebaseResp.user && firebaseResp.user.uid) {
+                  localStorage.setItem("userID", firebaseResp.user.uid);
+               }
+               this.firebaseService.getUserIDToken().then((res) => {
+                  this.dataService.getCustomTokenFromServer(res).subscribe((customToken) => {
+                     this.dataService.initAuthToken(customToken["token"]);
+                     localStorage.setItem("idToken", customToken["token"]);
+                     localStorage.setItem("loginDate", new Date().toUTCString());
+                     this.state = this.loginStates.ready;
+                     this.getSelectedMealPlan();
+                  });
+               });
+            })
+            .catch((error) => {
+               this.state = this.loginStates.error;
+               this.enableForm();
+               this.errorMessage = error.message;
             });
+      }
+   }
 
-    }
+   private getSelectedMealPlan() {
+      this.dataService.getSelectedMealPlanFromServer().subscribe(
+         (res) => {
+            this.dataHandlingService.handleMealSlotData(res).then((res: MealSlot[]) => {
+               if (res) {
+                  this.dataService.setMealSlots(res);
+                  this.router.navigateByUrl("splash");
+               } else {
+                  this.router.navigateByUrl("onboarding", { replaceUrl: true });
+               }
+               this.enableForm();
+            });
+         },
+         (exception) => {
+            // need to check if server return 'Unable to authenticate'
+            if (exception && exception.error && typeof exception.error == "string") {
+               const strRes = <string>exception.error;
+               if (strRes.includes("Unable to authenticate")) {
+                  this.state = this.loginStates.error;
+                  this.enableForm();
+                  this.errorMessage = "Internal server error";
+                  this.firebaseService.logoutUserFromApp().then(() => {
+                     //this.router.navigateByUrl('login');
+                  });
+               }
+            }
 
-    checkPasswords(group: FormGroup) {
-        let fieldsMatch = null;
-        let field1 = group.get('password').value;
-        let field2 = group.get('confirmPassword').value;
-        (field1 === field2) ? fieldsMatch = true : fieldsMatch = { notSame: true }
-        return fieldsMatch;
-    }
+            this.router.navigateByUrl("onboarding", { replaceUrl: true });
+         }
+      );
+   }
 
-    register() {
-        if (this.registerForm.valid) {
-            this.state = this.loginStates.loading;
-            this.firebaseService.createUserWithEmailAndPassword(this.registerForm.value.email, this.registerForm.value.password)
-                .then((res) => {
-                    const firebaseResp = <any>res;
-                    if(firebaseResp && firebaseResp.user && firebaseResp.user.uid){
-                        localStorage.setItem('userID', firebaseResp.user.uid)
-                    }
-                    this.firebaseService.getUserIDToken()
-                        .then((res) => {
-                            this.dataService.getCustomTokenFromServer(res).subscribe((customToken) => {
-                                this.dataService.initAuthToken(customToken['token']);
-                                localStorage.setItem('idToken', customToken['token']);
-                                localStorage.setItem('loginDate',new Date().toUTCString());
-                                this.state = this.loginStates.ready;
-                                this.router.navigateByUrl('onboarding', { replaceUrl: true });
-                            });
-                        })
-                })
-                .catch((error) => {
-                    this.state = this.loginStates.error;
-                    this.enableForm();
-                    this.errorMessage = error.message;
-                });
-        }
-    }
+   checkPasswords(group: FormGroup) {
+      let fieldsMatch = null;
+      let field1 = group.get("password").value;
+      let field2 = group.get("confirmPassword").value;
+      field1 === field2 ? (fieldsMatch = true) : (fieldsMatch = { notSame: true });
+      return fieldsMatch;
+   }
 
-    private disableForm() {
-        this.loginForm.disable();
-        this.formDisabled = true;
-    }
+   register() {
+      if (this.registerForm.valid) {
+         this.state = this.loginStates.loading;
+         this.firebaseService
+            .createUserWithEmailAndPassword(this.registerForm.value.email, this.registerForm.value.password)
+            .then((res) => {
+               const firebaseResp = <any>res;
+               if (firebaseResp && firebaseResp.user && firebaseResp.user.uid) {
+                  localStorage.setItem("userID", firebaseResp.user.uid);
+               }
+               this.firebaseService.getUserIDToken().then((res) => {
+                  this.dataService.getCustomTokenFromServer(res).subscribe((customToken) => {
+                     this.dataService.initAuthToken(customToken["token"]);
+                     localStorage.setItem("idToken", customToken["token"]);
+                     localStorage.setItem("loginDate", new Date().toUTCString());
+                     this.state = this.loginStates.ready;
+                     this.router.navigateByUrl("onboarding", { replaceUrl: true });
+                  });
+               });
+            })
+            .catch((error) => {
+               this.state = this.loginStates.error;
+               this.enableForm();
+               this.errorMessage = error.message;
+            });
+      }
+   }
 
-    private enableForm() {
-        this.loginForm.enable();
-        this.formDisabled = false;
-    }
+   private disableForm() {
+      this.loginForm.disable();
+      this.formDisabled = true;
+   }
+
+   private enableForm() {
+      this.loginForm.enable();
+      this.formDisabled = false;
+   }
 }
