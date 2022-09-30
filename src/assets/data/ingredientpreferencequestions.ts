3126f8eae298ee26cbc0a6ae51a2b339c905965a
Joel Dawson <joel@theprojectfactory.com>
2022-09-30 14:27:12 +1000

    feature: we now bold the most obvious selection choices

--- src/assets/data/ingredientpreferencequestions.ts
+++ src/assets/data/ingredientpreferencequestions.ts
@@ -1,48 +1,48 @@
 export const ingredientPreferenceQuestions = [
-  {
-    id: "familiarity_ratings",
-    title: "Familiarity",
-    options: [
-      {
-        id: 2,
-        title: "I know how this tastes",
-        selected: false,
-      },
-      {
-        id: 1,
-        title: "I have some idea how this tastes",
-        selected: false,
-      },
-      {
-        id: 0,
-        title: "I don't really know how this tastes",
-        selected: false,
-      },
-    ],
-    active: true,
-    disabled: false,
-  },
-  {
-    id: "taste_ratings",
-    title: "Taste",
-    options: [
-      {
-        id: 0,
-        title: "I like to eat this",
-        selected: false,
-      },
-      {
-        id: 1,
-        title: "I sometimes like to eat this",
-        selected: false,
-      },
-      {
-        id: 2,
-        title: "I don't like to eat this",
-        selected: false,
-      },
-    ],
-    active: false,
-    disabled: false,
-  },
+   {
+      id: "familiarity_ratings",
+      title: "Familiarity",
+      options: [
+         {
+            id: 2,
+            title: "I <strong>know</strong> how this tastes",
+            selected: false,
+         },
+         {
+            id: 1,
+            title: "I have <strong>some</strong> idea how this tastes",
+            selected: false,
+         },
+         {
+            id: 0,
+            title: "I <strong>don't</strong> really know how this tastes",
+            selected: false,
+         },
+      ],
+      active: true,
+      disabled: false,
+   },
+   {
+      id: "taste_ratings",
+      title: "Taste",
+      options: [
+         {
+            id: 0,
+            title: "I <strong>like</strong> to eat this",
+            selected: false,
+         },
+         {
+            id: 1,
+            title: "I <strong>sometimes like</strong> to eat this",
+            selected: false,
+         },
+         {
+            id: 2,
+            title: "I <strong>don't like</strong> to eat this",
+            selected: false,
+         },
+      ],
+      active: false,
+      disabled: false,
+   },
 ];
